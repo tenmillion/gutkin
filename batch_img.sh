b079bd05c05690876adc76f27f167007b4e5985a
Yoriko Yamamura <yoriko-y@is.naist.jp>
2013-09-23 15:03:02 +0900

    modified shell script to run on school cluster

--- batch_img.sh
+++ batch_img.sh
@@ -1,18 +1,19 @@
 # cat batch_img.sh
 i=1
-for exc in 0.001 0.005 0.010
+for exc in 0.0015
 do
-    for inh in 0.0005 0.0010 0.0050
+    for inh in 0.0005
     do
-	for aee in 0.001 0.005 0.010 #0.050
+	for aee in 0.010 0.050 0.100 0.150
 	do
-	    for aei in 0.005 0.010 #0.050
+	    for aei in 0.010 0.050 0.100 0.150
 	    do
-		for aie in 0.005 0.010 #0.050
+		for aie in 0.010 0.050 0.010 0.050
 		do
-		    for aii in 0.005 0.010 #0.050
+		    for aii in 0.010 0.050 0.010 0.050
 		    do
-			smt run -e python -m cobahh_synapse.py $exc $inh $aee $aei $aie $aii 3.0
+#			smt run -e python -m cobahh_synapse.py $exc $inh $aee $aei $aie $aii 3.0
+			~/python cobahh_synapse.py $exc $inh $aee $aei $aie $aii 3.0
 		    done
 		done
 	    done
