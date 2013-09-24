b7391ebacc194ee9410e9d91c26c621b02e2b3b0
Yoriko Yamamura <yoriko-y@is.naist.jp>
2013-09-24 10:22:42 +0900

    trying with less ee and more ei

--- batch.sh
+++ batch.sh
@@ -1,23 +1,14 @@
-# cat batch.sh
-for phi in 3.0 1.0 1.7320508075688772 5.196152422706632 9.0
+# cat batch2.sh
+for phi in 3.0 1.0 #1.7320508075688772 5.196152422706632 9.0
 do
-    for exc in 0.0015
+    for aee in 0.00001 0.00003 0.00005 0.005 0.01
     do
-	for inh in 0.0005
-	do
-	    for aee in 0.010 0.050 0.100 0.150
-	    do
-		for aei in 0.010 0.050 0.100 0.150
-		do
-		    for aie in 0.010 0.050 0.010 0.050
-		    do
-			for aii in 0.010 0.050 0.010 0.050
-			do
-			    ~/python batch_model.py $exc $inh $aee $aei $aie $aii $phi
-			 done
-		     done
-		 done
-	     done
-	 done
-     done
+   	for aei in 0.1 0.3 0.5 0.7 1.0
+       	do
+	    for pulse in 20 30 50
+       	    do
+            ~/python batch_model.py 0.0015 0.0002 $aee $aei 0.06 0.02 $phi $pulse
+	    done
+	done
+    done
 done
--- batch2.sh
+++ batch2.sh
@@ -1,13 +1,13 @@
 # cat batch2.sh
-for phi in 3.0 #1.0 1.7320508075688772 5.196152422706632 9.0
+for phi in 3.0 1.0 #1.7320508075688772 5.196152422706632 9.0
 do
-    for aee in 0.00010 0.00050 0.00100 0.0050 0.0100 0.05 0.1
+    for aee in 0.00001 0.00003 0.00005 0.005 0.01
     do
-   	for aei in 0.00010 0.00050 0.00100 0.0050 0.0100 0.05 0.1
+   	for aei in 0.1 0.3 0.5 0.7 1.0
        	do
-	    for pulse in 5 10 50 100 500
+	    for pulse in 20 30 50
        	    do
-		~/python batch_model.py 0.0015 0.0002 $aee $aei 0.06 0.02 $phi $pulse
+            ~/python batch_model.py 0.0015 0.0002 $aee $aei 0.06 0.02 $phi $pulse
 	    done
 	done
     done
